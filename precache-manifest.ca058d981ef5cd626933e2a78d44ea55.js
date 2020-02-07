ed3522888a77f169d3c1f452e2ae1569ed60fdb4
Gaith Mounajed <mhdgaith94@gmail.com>
2020-02-07 17:54:23 +0300

    Updates

---
+++ precache-manifest.ca058d981ef5cd626933e2a78d44ea55.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb831a59888ea8135ef63035d53e533e",
+    "url": "/firstReactProject/index.html"
+  },
+  {
+    "revision": "18079117cbd46a866d9b",
+    "url": "/firstReactProject/static/css/3.bccda483.chunk.css"
+  },
+  {
+    "revision": "bbfb49eed821a6802800",
+    "url": "/firstReactProject/static/css/5.c79f08b8.chunk.css"
+  },
+  {
+    "revision": "44a2cd70dd08035454d6",
+    "url": "/firstReactProject/static/css/main.241370d3.chunk.css"
+  },
+  {
+    "revision": "ed86d3ae67472f35810c",
+    "url": "/firstReactProject/static/js/2.46b73864.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/firstReactProject/static/js/2.46b73864.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18079117cbd46a866d9b",
+    "url": "/firstReactProject/static/js/3.5563de01.chunk.js"
+  },
+  {
+    "revision": "f08f7d930cb7f849c68a",
+    "url": "/firstReactProject/static/js/4.26eac5a2.chunk.js"
+  },
+  {
+    "revision": "11ba6bbe7acfcc6f0660e132a4496982",
+    "url": "/firstReactProject/static/js/4.26eac5a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbfb49eed821a6802800",
+    "url": "/firstReactProject/static/js/5.86b9a5f4.chunk.js"
+  },
+  {
+    "revision": "44a2cd70dd08035454d6",
+    "url": "/firstReactProject/static/js/main.3a4a2e97.chunk.js"
+  },
+  {
+    "revision": "63b50c3878319f4c0356",
+    "url": "/firstReactProject/static/js/runtime-main.e6c2c954.js"
+  },
+  {
+    "revision": "351c7e9c0b2dfbdb39917a5064890aa9",
+    "url": "/firstReactProject/static/media/clock.351c7e9c.png"
+  },
+  {
+    "revision": "1dccde0ffb6656c147d7aaf7cf673167",
+    "url": "/firstReactProject/static/media/heart.1dccde0f.png"
+  },
+  {
+    "revision": "ebf5cdcf108ad8166962ee9ff7d7af68",
+    "url": "/firstReactProject/static/media/loadingCircle.ebf5cdcf.png"
+  },
+  {
+    "revision": "6bad705b40cff336fdd88192495c4043",
+    "url": "/firstReactProject/static/media/logo.6bad705b.png"
+  },
+  {
+    "revision": "898000e128302c013fb0b53bb568317f",
+    "url": "/firstReactProject/static/media/signature.898000e1.png"
+  },
+  {
+    "revision": "3ef03bf110b1a81f5551ffd300441df1",
+    "url": "/firstReactProject/static/media/star.3ef03bf1.png"
+  },
+  {
+    "revision": "04eb2896a3b8246d869ef928135b80b1",
+    "url": "/firstReactProject/static/media/suitcase.04eb2896.png"
+  }
+]);
